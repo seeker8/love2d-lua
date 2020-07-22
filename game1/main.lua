3ac8e435e56a97202c3358dd12e10a27e27d6a35
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-07-22 19:57:04 -0400

    global and local variables

--- game1/main.lua
+++ game1/main.lua
@@ -4,6 +4,12 @@ function changeMessage()
   message = "This message was changed inside a function"
 end
 
+function increase(i)
+  local var = i
+  var = var * var
+  return var
+end
+
 -- conditionals
 condition = 20
 if condition > 0 then
@@ -11,8 +17,9 @@ if condition > 0 then
 end
 
 changeMessage()
+increase(2)
 
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
-  love.graphics.print(message)
+  love.graphics.print(var)
 end
