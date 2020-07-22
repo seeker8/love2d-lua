22df65a2fb30d2c114968ddaccaff33f96bb6df3
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-07-22 17:29:41 -0400

    variable usage

--- game1/main.lua
+++ game1/main.lua
@@ -1,4 +1,12 @@
 message = "Hello World!"
+
+-- conditionals
+condition = 20
+if condition > 0 then
+  message = "it is greater than 0"
+end
+
+
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
   love.graphics.print(message, 200, 300)
