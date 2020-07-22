807bcfe3d4ee5d730ec7a400b632ff9d80d5519a
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-07-22 18:52:14 -0400

    functions usage

--- game1/main.lua
+++ game1/main.lua
@@ -1,13 +1,18 @@
 message = "Hello World!"
 
+function changeMessage()
+  message = "This message was changed inside a function"
+end
+
 -- conditionals
 condition = 20
 if condition > 0 then
   message = "it is greater than 0"
 end
 
+changeMessage()
 
 function love.draw()
   love.graphics.setFont(love.graphics.newFont(50))
-  love.graphics.print(message, 200, 300)
+  love.graphics.print(message)
 end
