2275b8feae337731d44e59ed426352ae4644f0f1
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-07-25 16:34:48 -0400

    add initial clicker game code

---
+++ clicker/main.lua
@@ -0,0 +1,16 @@
+
+function love.load()
+  
+end
+
+-- dt -> delta time
+function love.update(dt)
+  
+end
+
+
+function love.draw()
+  love.graphics.setColor(0, 0, 1)
+  love.graphics.rectangle("fill", 200, 100, 200, 100)
+  love.graphics.circle("fill", 59, 50, 10)
+end
