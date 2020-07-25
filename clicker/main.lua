b93b30cbbee16c8f411bd1fe250d55c913a90e1d
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-07-25 17:46:29 -0400

    add labels to time and score

--- clicker/main.lua
+++ clicker/main.lua
@@ -7,44 +7,60 @@ function love.load()
   button.size = 50 
 
   score = 0
-  timer = 10 
+  timer = 10
+  gameState = 1 
 
   myFont = love.graphics.newFont(40)
 end
 
 -- dt -> delta time
 function love.update(dt)
-  if timer > 0 then
-    timer = timer - dt
-  end
+  if gameState == 2 then
+    if timer > 0 then
+      timer = timer - dt
+    end
 
-  if timer < 0 then
-    timer = 0
+    if timer < 0 then
+      timer = 0
+      gameState = 1
+    end
   end
 end
 
 
 function love.draw()
-  love.graphics.setColor(0, 0, 1)
-  love.graphics.circle("fill", button.x, button.y, button.size)
+  if gameState == 2 then
+    love.graphics.setColor(0, 0, 1)
+    love.graphics.circle("fill", button.x, button.y, button.size)
+  end
+
+  if gameState == 1 then
+    love.graphics.printf("Click anywhere to begin", 0 , love.graphics.getHeight()/2, love.graphics.getWidth(), "center")
+  end
 
   -- score
   love.graphics.setFont(myFont)
   love.graphics.setColor(1, 1, 1)
-  love.graphics.print(score)
+  love.graphics.print("Score: " .. score)
 
   -- timer
-  love.graphics.print(math.ceil(timer), 200, 0)
+  love.graphics.print("Time: " .. math.ceil(timer), 350, 0)
 end
 
 function love.mousepressed(x, y, buttonClicked, istouch)
-  if buttonClicked == 1 then
+  if buttonClicked == 1 and gameState == 2  then
     d = distance(button.x, button.y, x, y)
     if d <= button.size then
       score = score + 1
       button.x = math.random(button.size, love.graphics.getWidth() - button.size)
       button.y = math.random(button.size, love.graphics.getHeight() - button.size)
     end
+  end
+
+  if gameState == 1 then
+    gameState = 2
+    timer = 10
+    score = 0
   end
 end
 
