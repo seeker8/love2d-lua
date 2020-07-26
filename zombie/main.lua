d8c8d51f2d37419dda5ffb5bc7fd8b0896711911
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-07-26 16:00:41 -0400

    add movement to player

---
+++ zombie/main.lua
@@ -0,0 +1,37 @@
+
+function love.load()
+  sprites = {}
+  sprites.player = love.graphics.newImage('sprites/player.png')
+  sprites.bullet = love.graphics.newImage('sprites/bullet.png')
+  sprites.zombie = love.graphics.newImage('sprites/zombie.png')
+  sprites.background = love.graphics.newImage('sprites/background.png')
+
+  -- player
+  player = {}
+  player.x = 200
+  player.y = 200
+  player.speed = 180
+end
+
+function love.update(dt)
+  if love.keyboard.isDown("s") and player.y <= love.graphics.getHeight() - 45 then
+    player.y = player.y + player.speed * dt
+  end
+
+  if love.keyboard.isDown("w") and player.y > 0 then
+    player.y = player.y - player.speed * dt
+  end
+
+  if love.keyboard.isDown("a") and player.x > 0 then
+    player.x = player.x - player.speed * dt
+  end
+
+  if love.keyboard.isDown("d") and player.x <= love.graphics.getWidth() - 30 then
+    player.x = player.x + player.speed * dt
+  end
+end
+
+function love.draw()
+  love.graphics.draw(sprites.background, 0, 0)
+  love.graphics.draw(sprites.player, player.x, player.y)
+end
