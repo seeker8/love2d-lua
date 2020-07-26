5889b192b0d3dac7447d7261535009fb8a72f9f2
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-07-26 16:39:14 -0400

    make image face mouse pointer

--- zombie/main.lua
+++ zombie/main.lua
@@ -33,5 +33,9 @@ end
 
 function love.draw()
   love.graphics.draw(sprites.background, 0, 0)
-  love.graphics.draw(sprites.player, player.x, player.y)
+  love.graphics.draw(sprites.player, player.x, player.y, getAngleInRadians(),nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+end
+
+function getAngleInRadians()
+  return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
 end
