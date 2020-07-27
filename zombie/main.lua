bfcee31d0c01f62e13b92be2e4e7d229e8bf1c92
seeker8 <chuyo_gk87@yahoo.com.mx>
2020-07-27 10:24:38 -0400

    random spawn position for zombies

--- zombie/main.lua
+++ zombie/main.lua
@@ -114,10 +114,26 @@ end
 
 function spawnZombie()
   zombie = {}
-  zombie.x = math.random(0, love.graphics.getWidth())
-  zombie.y = math.random(0, love.graphics.getHeight())
+  zombie.x = 0
+  zombie.y = 0
   zombie.speed = 100
   zombie.dead = false
+
+  local side = math.random(1, 4)
+
+  if side == 1 then
+    zombie.x = -30
+    zombie.y = math.random(0, love.graphics.getHeight())
+  elseif side == 2 then
+    zombie.x = math.random(0, love.graphics.getWidth())
+    zombie.y = -30
+  elseif side == 3 then
+    zombie.x = love.graphics.getWidth() + 30
+    zombie.y = math.random(0, love.graphics.getHeight())
+  else
+    zombie.x = math.random(0, love.graphics.getWidth())
+    zombie.y = love.graphics.getHeight() + 30
+  end
 
   table.insert(zombies, zombie)
 end
